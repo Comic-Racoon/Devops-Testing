d74cf5e2ac4c2811a3dcb3e1daf3a4f23bad57dc
Rahul Umesh Mhatre <rahul.mhatre@phenompeople.com>
2024-06-07 22:53:51 +0530

    updated pattern

---
+++ Code/Python/A-Z Striver Theory & Problems/01_Basics/02_Pattern Problems/10_Half Diamond Star Pattern.py
@@ -0,0 +1,27 @@
+'''
+Input Format: N = 3
+Result: 
+  *  
+  **
+  ***  
+  **
+  *   
+Input Format: N = 6
+Result:   
+     *
+     **
+     *** 
+     ****
+     *****
+     ******  
+     *****
+     ****
+     ***    
+     **
+     *
+'''
+
+#solution
+
+N = 3
+
