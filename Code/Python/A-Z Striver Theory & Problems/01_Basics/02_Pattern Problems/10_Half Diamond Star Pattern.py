655d6590454d7257f31dd912329ab4372a9030c1
Rahul Umesh Mhatre <rahul.mhatre@phenompeople.com>
2024-06-08 10:29:57 +0530

    added pattern problem

--- Code/Python/A-Z Striver Theory & Problems/01_Basics/02_Pattern Problems/10_Half Diamond Star Pattern.py
+++ Code/Python/A-Z Striver Theory & Problems/01_Basics/02_Pattern Problems/10_Half Diamond Star Pattern.py
@@ -25,3 +25,24 @@ Result:
 
 N = 3
 
+for i in range(1,(N*2)):
+    
+    if i <= N :
+      for j in range(1,i+1):
+         print("*", end="")
+    
+    else:
+      for j in range(1, (2 * N) - i + 1):
+        print("*", end="")
+
+    print()
+    
+
+'''
+Explanation:
+
+    Loop Control: The loop runs from 1 to 2N - 1. This creates the first half (increasing stars) and the second half (decreasing stars) of the pattern.
+    Increasing Stars: When i is less than or equal to N, it prints stars from 1 to i.
+    Decreasing Stars: When i is greater than N, it prints stars from 1 to (2 * N) - i.
+
+'''
